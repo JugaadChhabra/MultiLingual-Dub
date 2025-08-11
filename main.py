ffa1beae7c11270357e5f7f0dfee741675a817e5
Jugaad Chhabra <jugaad.chhabra@gmail.com>
2025-08-12 02:04:41 +0530

    added basic assembly ai api code

---
+++ main.py
@@ -0,0 +1,17 @@
+# Install the assemblyai package by executing the command "pip install assemblyai"
+
+import assemblyai as aai
+
+aai.settings.api_key = "1d7a922389144d1aba29077899b40e6f"
+
+# audio_file = "./local_file.mp3"
+audio_file = "https://assembly.ai/wildfires.mp3"
+
+config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
+
+transcript = aai.Transcriber(config=config).transcribe(audio_file)
+
+if transcript.status == "error":
+  raise RuntimeError(f"Transcription failed: {transcript.error}")
+
+print(transcript.text)
