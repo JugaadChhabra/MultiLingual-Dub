c40a95dba2c31368c61ac426310102d73a01ac1f
Jugaad Chhabra <jugaad.chhabra@gmail.com>
2025-08-12 23:48:39 +0530

    added stt and mp4 to mp3

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Install the assemblyai package by executing the command "pip install assemblyai"
-
+# from moviepy.editor import *
 import assemblyai as aai
 
 aai.settings.api_key = "1d7a922389144d1aba29077899b40e6f"
 
 # audio_file = "./local_file.mp3"
-audio_file = "https://assembly.ai/wildfires.mp3"
+audio_file = "example.mp3"
 
 config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
 
@@ -15,3 +15,7 @@ if transcript.status == "error":
   raise RuntimeError(f"Transcription failed: {transcript.error}")
 
 print(transcript.text)
+
+# video = VideoFileClip("SampleVideo.mp4")
+
+# video.audio.write_audiofile("example.mp3")
