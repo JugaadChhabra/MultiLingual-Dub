07932a293a8bb312afcc8788596578e1e466649b
Jugaad Chhabra <jugaad.chhabra@gmail.com>
2025-08-13 22:58:11 +0530

    added funcitons for mp3 to mp4 and transcription

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-# Install the assemblyai package by executing the command "pip install assemblyai"
-# from moviepy.editor import *
+from moviepy.editor import *
 import assemblyai as aai
 
-aai.settings.api_key = "1d7a922389144d1aba29077899b40e6f"
+def transcribe_audio(filepath):
+  aai.settings.api_key = "1d7a922389144d1aba29077899b40e6f"
+  
+  config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
 
-# audio_file = "./local_file.mp3"
-audio_file = "example.mp3"
+  transcript = aai.Transcriber(config=config).transcribe(filepath)
+  if transcript.status == "error":
+    raise RuntimeError(f"Transcription failed: {transcript.error}")
 
-config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.best)
+  print(transcript.text)
 
-transcript = aai.Transcriber(config=config).transcribe(audio_file)
+def mp4_to_mp3(filename):
+  video = VideoFileClip(filename)
+  video.audio.write_audiofile("example.mp3")
 
-if transcript.status == "error":
-  raise RuntimeError(f"Transcription failed: {transcript.error}")
-
-print(transcript.text)
-
-# video = VideoFileClip("SampleVideo.mp4")
-
-# video.audio.write_audiofile("example.mp3")
+mp4_to_mp3("SampleVideo.mp4")
+transcribe_audio("example.mp3")
